7b6c57c3300269eaeac36a2dd3df7187deb6010b
Pratik Shankar <pratikshankar@Pratiks-MacBook-Air.local>
2025-04-29 12:46:51 +0530

    initial python notebook created vectors updated and sample query ran.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Pratik Shankar',
+    author_email= 'pratik01.shankar@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
